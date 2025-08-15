bd1742de8dba49b2a3f9634cc1d35cde28fe2fda
sandy@sandy534 <allams04sandeep@gmail.com>
2025-08-15 13:06:15 -0400

    my first commit

---
+++ Vennela/Hello_World.java
@@ -0,0 +1,11 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        String name = "Lahitya";
+        int a = 1;
+        double b = 11.99;
+
+        System.out.println("Her Name is : " + name);
+    }
+}
